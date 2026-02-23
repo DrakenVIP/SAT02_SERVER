84166ebb1d6fd5e6d2aac8f06e5b74138e8352c8
Draken <Starlingliriano@gmail.com>
2026-02-22 20:57:15 -0400

    se elimino codigo

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("io.ktor:ktor-server-core:2.3.7")
     implementation("io.ktor:ktor-server-netty:2.3.7")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("io.ktor:ktor-client-cio:2.3.7")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
 }
 application {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,9 @@ import io.ktor.server.routing.*
 
 fun main() {
 
+    val token ="EAAbPAueSpAUBQ15gG57Mas7q9iYSbDRu8AqgS7xlzWZAplsYmXqFTYqESbYvRxfAZBybXTutZCwDMJ3ijj44A3mM1swuBs2Qktjz9YswlZAABGRi69tpK0dxV0RzXlCByiwTyZBXZB4hUglU4efWxo0ciOClVGEvwJdOe3KqmxsDhneoTin549L5PPZC8aV9fFljgZA7ANzmCLmKHZCXsk6FaAzDOTicai7AGHDU6E0Ea9AYy57ZAmOhSYdpHVohUHaumYFQvhMYqytDGUV8o9AWHh"
+    val numberId ="971935982677896"
+    val verifyToken = "Maria123#"
     val port = System.getenv("PORT")?.toInt() ?: 8080
 
     embeddedServer(Netty, port = port) {
